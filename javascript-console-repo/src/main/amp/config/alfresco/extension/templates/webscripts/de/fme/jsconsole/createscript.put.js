5fd4eab5d73f8490acfa74f7c060baf73848c3d3
Florian Maul <florian.maul@gmail.com>
2013-09-14 09:19:53 +0200

    Added mimetype for saved scripts, fixes #3

--- javascript-console-repo/src/main/amp/config/alfresco/extension/templates/webscripts/de/fme/jsconsole/createscript.put.js
+++ javascript-console-repo/src/main/amp/config/alfresco/extension/templates/webscripts/de/fme/jsconsole/createscript.put.js
@@ -3,4 +3,5 @@ var scriptFolder = companyhome.childrenByXPath("app:dictionary/app:scripts")[0];
 if (scriptFolder) {
 	var scriptNode = scriptFolder.createFile(args.name);
 	scriptNode.content = requestbody.content;
+	scriptNode.mimetype = "application/x-javascript";
 }
