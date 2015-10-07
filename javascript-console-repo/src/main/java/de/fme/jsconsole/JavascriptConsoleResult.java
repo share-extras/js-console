cce6c9e2f862563c947ff4313caa8d02e0404cc1
Lukasz Racon <lracon@jgsullivan.com>
2015-10-07 10:56:03 -0500

    remove duplicate json output

--- javascript-console-repo/src/main/java/de/fme/jsconsole/JavascriptConsoleResult.java
+++ javascript-console-repo/src/main/java/de/fme/jsconsole/JavascriptConsoleResult.java
@@ -44,12 +44,10 @@ public class JavascriptConsoleResult extends JavascriptConsoleResultBase {
 		try {
 			JSONObject jsonOutput = generateJsonOutput();
 			response.getWriter().write(jsonOutput.toString());
-
 		} catch (JSONException e) {
 			throw new WebScriptException(Status.STATUS_INTERNAL_SERVER_ERROR,
 					"Error writing json response.", e);
 		}
-		this.writeJson(response, getPrintOutput());
 	}
 
 	/**
